e1639df26bc0aaf39982141c69247c77cc86ae98
Lukas <computorgenius101@gmail.com>
2021-06-29 20:55:19 -0700

    ✨ dot is alias for index.pog

--- bin/index.js
+++ bin/index.js
@@ -2,8 +2,9 @@
 const fs = require('fs');
 const exec = require('./src/exec');
 const Environment = require('./src/environment');
+const yargs = require('yargs');
 const argv = require('yargs')
-	.scriptName('intui')
+	.scriptName('pog')
 	.usage('Usage: $0 <filename>')
 	.alias('h', 'help')
 	.alias('v', 'version')
@@ -14,9 +15,18 @@ const argv = require('yargs')
 	})
 	.parse();
 
-if (argv._.length == 0) {
-	require('./src/repl');
-} else if (argv._.length == 1) {
-	const file = fs.readFileSync(argv._[0]).toString();
-	exec(file, false, argv.d, new Environment());
+switch (argv._.length) {
+	case 0:
+		require('./src/repl');
+		break;
+
+	case 1:
+		const name = argv._[0] == '.' ? 'index.pog' : argv._[0] + '.pog';
+		const file = fs.readFileSync(name).toString();
+		exec(file, false, argv.d, new Environment());
+		break;
+
+	default:
+		yargs.showHelp();
+		break;
 }
